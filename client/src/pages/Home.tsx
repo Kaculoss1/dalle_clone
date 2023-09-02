4a92b0434a784d971f301ae71c343879625bf11c
Kaculoss1 <Sanialhassan853@gmail.com>
2023-09-02 22:49:11 +0000

    changed server url

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -28,10 +28,13 @@ const Home = () => {
       setloading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-        });
+        const response = await fetch(
+          "https://dall-e-5d0p.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
     app.listen(8080, () =>
-      console.log("Server has started on port http://localhost:8080")
+      console.log("Server has started on https://dall-e-5d0p.onrender.com")
     );
   } catch (error) {
     console.log(error);
